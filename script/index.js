bd69d661695ef3f0caee9225a5c843c54cddb846
Rafael <rafael111515@gmail.com>
2022-04-01 18:57:37 -0300

    Mudança nos contatos e na ScrollBar

--- script/index.js
+++ script/index.js
@@ -1,6 +1,25 @@
 //Função para o menu subir quando um item for clicado
-$(document).ready(function(){
-    $(".navbar-nav ").click(function(event){
-      $(".navbar-collapse").collapse('hide');
-    });
+$(document).ready(function () {
+  $(".navbar-nav ").click(function (event) {
+    $(".navbar-collapse").collapse('hide');
   });
+});
+
+
+$("#arrow").click(function () {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  return false;
+
+});
+
+
+const scrollUp = document.getElementById('arrow')
+
+window.addEventListener('scroll', () =>{
+  if(window.pageYOffset > 100){
+    scrollUp.classList.add('up')
+  }else{
+    scrollUp.classList.remove('up')
+  }
+
+})
